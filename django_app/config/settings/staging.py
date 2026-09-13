e7c3a358976a1aee76461ec634ca6a60693f7306
Sonal Karmakar <sonalkarmakar.gaining400@slmails.com>
2026-09-13 10:27:24 +0530

    Added sample files for Django security check

---
+++ django_app/config/settings/staging.py
@@ -0,0 +1,16 @@
+# django_app/config/settings/staging.py
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+
+# Settings that `manage.py check --deploy` validates
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = "DENY"
---
+++ django_app/config/urls.py
@@ -0,0 +1,4 @@
+# django_app/config/urls.py
+from django.urls import path
+
+urlpatterns = []
